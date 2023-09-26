bf4d9acba14d95919c9ee01dbe338f60877f7e7a
SantosJGND <dourado.jns@gmail.com>
2023-09-26 14:37:51 +0100

    add is_delete field to fileysstem model

--- collect_files/models.py
+++ collect_files/models.py
@@ -12,5 +12,7 @@ class FileInSystem(models.Model):
     file_date = models.DateTimeField('date published')
     file_hash = models.CharField(max_length=200)
 
+    is_deleted = models.BooleanField(default=False)
+
     def __str__(self):
         return self.file_name
